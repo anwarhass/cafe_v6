2801ed74da59006632fc073e169af3d621f8d902
anwarhass <125407453+anwarhass@users.noreply.github.com>
2023-03-21 12:04:05 +1300

    test

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template
+import Sqlite3
+from sqlite3 import Error
+
+DATABASE = "C:\Users\19467\OneDrive - Wellington College\Flask\cafe_v5\coffee"
+
+app = Flask(__name__)
+
+def create_connection(db_file):
+    """
+    Create a connection with the database
+    parameter: name of the database file
+    returns: a connection to the file
+    """
+    try :
+        connection = sqlite3.s=connection(db.file)
+        return connection
+    except Error as e:
+        print(e)
+    return none
+
+
+@app.route('/')
+def render_home_page():
+    return render_template('home.html')
+
+
+@app.route('/menu')
+def render_menu_page():
+    con = create_connection(DATABASE)
+    query = "SELECT name, discription, volume, image, price FROM products"
+    cur = con()
+    cur.excecute(query)
+    product_list = cur.fecthall()
+    con.close()
+    print(product_list)
+    return render_template('menu.html', products=product_list)
+
+
+@app.route('/contact')
+def render_contact_page():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run()
+#app.run(host='0.0.0.0', debug=True)
