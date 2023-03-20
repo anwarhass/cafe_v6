18b8268a9846dea60eed29bac070099da19fd8aa
anwarhass <125407453+anwarhass@users.noreply.github.com>
2023-03-21 12:22:32 +1300

    fixed bugs menu is now displaying

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 import sqlite3
 from sqlite3 import Error
 
-#DATABASE = "C:\Users\19467\OneDrive - Wellington College\Flask\cafe_v5\coffee"
+DATABASE = "C:/Users/19467/PycharmProjects/flaskProject/coffee_db"
 #this is a test comment
 app = Flask(__name__)
 
@@ -29,9 +29,9 @@ def render_home_page():
 def render_menu_page():
     con = create_connection(DATABASE)
     query = "SELECT name, description, volume, image, price FROM products"
-    cur = con()
-    cur.excecute(query)
-    product_list = cur.fecthall()
+    cur = con.cursor()
+    cur.execute(query)
+    product_list = cur.fetchall()
     con.close()
     print(product_list)
     return render_template('menu.html', products=product_list)
